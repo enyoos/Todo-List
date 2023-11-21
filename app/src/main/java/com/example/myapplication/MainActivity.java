e05f316d7814c602793a8abbf4f43aeafa0a10c1
enyoos <lebedevhh@outlook.fr>
2023-11-21 15:07:09 -0500

    nailed the first project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,28 +8,50 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static ArrayList<String> todos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EditText inputField = findViewById(R.id.inputNameField);
-        TextView outputField = findViewById(R.id.outputNameField);
+        ListView lV         = findViewById(R.id.listView);
         Button btn = findViewById(R.id.buttonEnter);
+
+        // listView is just a list of text content
+        ArrayAdapter<String> arr = new ArrayAdapter<>(this, R.layout.white_txt_lv, R.id.list_content, todos);
+        lV.setAdapter(arr);
+
+        lV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ArrayAdapter arr = ( ArrayAdapter ) adapterView.getAdapter();
+                deleteTodo(arr, i);
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // get the name from the input field
                 String name = inputField.getText().toString();
-                registerName(name, MainActivity.this , outputField);
+                addTodo(name, MainActivity.this , lV);
                 inputField.setText("");
             }
         });
@@ -44,16 +66,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void registerName (String name, Context ctx, TextView tv )
+    public void deleteTodo(ArrayAdapter<String> arr , int idx )
     {
-        String temp = tv.getText().toString();
-        String rgxp = ":";
-        String out = temp.split(rgxp)[0];
+        String toRemove = arr.getItem(idx);
+        arr.remove(toRemove);
+        arr.notifyDataSetChanged();
 
-        if ( !name.isEmpty() )
+        String out = String.format("removed : %s at the idx %d", toRemove, idx);
+        Log.d ( "[MESSAGE]", out);
+    }
+
+    public void addTodo(String content, Context ctx, ListView lV )
+    {
+        if ( !content.isEmpty() )
         {
-            out += ": " + name;
-            tv.setText(out);
+            // add the list to the thing
+            todos.add(content);
+            (( ArrayAdapter<String>) lV.getAdapter()).notifyDataSetChanged();
+
+            String out = String.format("added %s to the arr", content);
+            Log.d("[MESSAGE]", out);
         }
         else
         {
